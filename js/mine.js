8247780023a11708c6535d0e1ca5590748e01973
MohsenMamdouhGawish <Mohsenmamdouh@github.com>
2018-09-11 21:05:58 +0300

    Home page

---
+++ js/mine.js
@@ -0,0 +1,13 @@
+$(".show").click(function(){
+    $("#contentHidden").css("display","block");
+    $("#form").css("display","none");
+})
+/*$(document).ready(function(){
+    $("#loading").css("opacity","0")
+    $("body").css("overflow","auto")
+
+})*/
+
+
+
+
