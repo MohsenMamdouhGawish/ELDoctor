8e157b9f70435c27feb19b7ff8019c53f5a1975c
MohsenMamdouhGawish <mohsenmamdouh114@gmail.com>
2018-09-17 16:49:39 +0200

    42_pages

--- js/mine.js
+++ js/mine.js
@@ -1,6 +1,10 @@
+$("#contentHidden").slideUp();
 $(".show").click(function(){
-    $("#contentHidden").css("display","block");
-    $("#form").css("display","none");
+    $("#form").slideUp(1000,function(){
+                $("#contentHidden").slideDown(4000);
+
+    });
+
 })
 $(document).ready(function(){
     $("#loading").fadeOut(1000);
@@ -13,10 +17,54 @@ $(document).ready(function(){
     if ($(document).scrollTop() >100) {
       $(".hiddenNav").animate({top:'0'},10)
     } else {
-      $(".hiddenNav").animate({top:'-110px'},10)
+      $(".hiddenNav").animate({top:'-300px'},10)
     }
   });
 });
 
 
 
+
+
+
+var dangers = document.getElementsByClassName("alert-danger")
+var success = document.getElementsByClassName("alert-success")
+
+function validateUsername()
+{
+       var userName = document.getElementById("username").value
+
+    var r = /^[A-z]{12,18}$/;
+ 
+    if(r.test(userName) == false)
+        {
+            success[0].style.display="none"
+            dangers[0].style.display="block"
+        }
+    else
+        {
+            success[0].style.display="block"
+            dangers[0].style.display="none"
+
+        }
+}
+
+
+ function validatePassword()  {
+             var Password = document.getElementById("password").value
+
+ 
+    var r1 = /^[0-9]{6,10}/;
+      if(r1.test(Password) == false)
+        {
+            console.log('sss');
+            success[1].style.display="none"
+            dangers[1].style.display="block"
+        }
+    else
+        {
+            success[1].style.display="block"
+            dangers[1].style.display="none"
+
+        }
+ }
